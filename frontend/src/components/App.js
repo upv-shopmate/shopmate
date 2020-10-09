e348c2077a512bc284e77ed151b911b981451c0f
Pere Díaz <pere-altea@hotmail.com>
2020-10-09 17:12:50 +0200

    boiler-plate-electron-react

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import '../assets/css/App.css';
+import React, { Component } from 'react';
+import electron from 'electron';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hey</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
