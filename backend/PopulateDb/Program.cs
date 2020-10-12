7bf1b6740375e097eeb54835069ba4001876a079
SaltyKawaiiNeko <software@skneko.moe>
2020-10-12 10:50:12 +0200

    Add scrapOpenFoodFacts submodule

---
+++ backend/PopulateDb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PopulateDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
