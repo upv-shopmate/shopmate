19c836c908b0138cd01819229dd454e4f441ff7c
SaltyKawaiiNeko <software@skneko.moe>
2020-10-14 19:44:54 +0200

    Add code to seed database

---
+++ backend/PopulateDb/ProductJsonDAO.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace PopulateDb
+{
+    internal class ProductJsonDAO
+    {
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+
+        [JsonProperty("weight")]
+        public double? Weight { get; internal set; }
+
+        [JsonProperty("volume")]
+        public double? Volume { get; internal set; }
+
+        [JsonProperty("units")]
+        public ushort? Units { get; internal set; }
+
+        [JsonProperty("origin_country")]
+        public string OriginCountry { get; internal set; }
+
+        [JsonProperty("edible")]
+        public bool Edible { get; internal set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; internal set; }
+
+        [JsonProperty("images")]
+        public List<string> Pictures { get; internal set; }
+
+        [JsonProperty("available_stock")]
+        public uint? AvailableStock { get; internal set; }
+
+        [JsonProperty("times_sold")]
+        public uint TimesSold { get; internal set; }
+
+        [JsonProperty("brands")]
+        public List<string> Brands { get; internal set; }
+
+        [JsonProperty("categories")]
+        public List<string> Categories { get; internal set; }
+
+        [JsonProperty("labels")]
+        public List<string> Labels { get; internal set; }
+    }
+}
