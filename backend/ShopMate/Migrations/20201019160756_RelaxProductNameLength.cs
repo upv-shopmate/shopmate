0b2a0f3d1c5e46952ebe4ac6f21c4454395bfaf0
SaltyKawaiiNeko <software@skneko.moe>
2020-10-19 00:30:22 +0200

    Relax restrictions in static model

---
+++ backend/ShopMate/Migrations/20201019160756_RelaxProductNameLength.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopMate.Migrations
+{
+    public partial class RelaxProductNameLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(120)",
+                oldMaxLength: 120);
+        }
+    }
+}
