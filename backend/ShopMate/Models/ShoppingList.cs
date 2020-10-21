43e1b1df767ffd595d71280a7644a7d4033ec447
Álvaro Gómez Cuenca <basura1200@gmail.com>
2020-10-21 11:36:10 +0200

    Creado y codificado ShoppingList.cs y ShoppingListEntry.cs

---
+++ backend/ShopMate/Models/ShoppingList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopMate.Models
+{
+    public class ShoppingList : IEquatable<ShoppingList>
+    {
+        public int Id { get; private set; }
+
+        [MaxLength(50)]
+        public string Name { get; internal set; }
+
+        public IReadOnlyCollection<ShoppingListEntry> Entries { get => entries; }
+
+        readonly HashSet<ShoppingListEntry> entries = new HashSet<ShoppingListEntry>();
+
+        public ShoppingList(string name)
+        {
+            Name = name;
+        }
+
+        public void ChangeName(string newName) => Name = newName;
+
+        public void AddProduct(ShoppingListEntry entry)
+        {
+            if (entries.TryGetValue(entry, out ShoppingListEntry? currentEntry))
+            {
+                currentEntry.Quantity += entry.Quantity;
+            } 
+            else
+            {
+                entries.Add(entry);
+            }
+        }
+
+        public void DeleteProduct(ShoppingListEntry entry)
+        {
+            if (entries.TryGetValue(entry, out ShoppingListEntry? currentEntry))
+            {
+                if (currentEntry.Quantity <= entry.Quantity)
+                {
+                    entries.Remove(currentEntry);
+                }
+                else
+                {
+                    currentEntry.Quantity -= entry.Quantity;
+                }
+            }
+        }
+
+        public void DeleteProduct(Product product)
+        {
+
+        }
+
+        public override bool Equals(object? other) => other is ShoppingList list && Equals(list);
+
+        public bool Equals(ShoppingList? other) => Id == other?.Id;
+
+        public static bool operator ==(ShoppingList lhs, ShoppingList rhs) => lhs.Equals(rhs);
+        public static bool operator !=(ShoppingList lhs, ShoppingList rhs) => !lhs.Equals(rhs);
+
+        public override int GetHashCode() => Name.GetHashCode();
+    }
+}
---
+++ backend/ShopMate/Models/ShoppingListEntry.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopMate.Models
+{
+    [Owned]
+    public class ShoppingListEntry : IEquatable<ShoppingListEntry>
+    {
+        public int Quantity { get; internal set; }
+
+        public Product Product { get; internal set; }
+
+        public ShoppingListEntry(int quantity, Product product)
+        {
+            Quantity = quantity;
+            Product = product;
+        }
+
+        public override bool Equals(object? other) => other is ShoppingListEntry entry && Equals(entry);
+
+        public bool Equals(ShoppingListEntry? other) => Product == other?.Product!;
+
+        public static bool operator ==(ShoppingListEntry lhs, ShoppingListEntry rhs) => lhs.Equals(rhs);
+        public static bool operator !=(ShoppingListEntry lhs, ShoppingListEntry rhs) => !lhs.Equals(rhs);
+
+        public override int GetHashCode() => Product.GetHashCode();
+    }
+}
