273cf81d569758bbc8bd139b2d15b505029aab33
SaltyKawaiiNeko <software@skneko.moe>
2020-10-31 13:45:42 +0100

    Add migration ShoppingLists

---
+++ backend/ShopMate/Migrations/20201031120526_ShoppingLists.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopMate.Migrations
+{
+    public partial class ShoppingLists : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SubtotalPrice = table.Column<decimal>(type: "money", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PriceModifierBreakdown",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShoppingListId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriceModifierBreakdown", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PriceModifierBreakdown_ShoppingLists_ShoppingListId",
+                        column: x => x.ShoppingListId,
+                        principalTable: "ShoppingLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PriceModifierBreakdown_ShoppingListId",
+                table: "PriceModifierBreakdown",
+                column: "ShoppingListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PriceModifierBreakdown");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingLists");
+        }
+    }
+}
