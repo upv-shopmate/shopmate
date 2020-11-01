15386eead3621a1c9a0bb50ee533a5b226e0dcb1
Alberto Baixauli <albaiher99@gmail.com>
2020-11-01 20:40:14 +0100

    Add MapController and migration

---
+++ backend/ShopMate/Migrations/20201101193325_map.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopMate.Migrations
+{
+    public partial class map : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Map",
+                table: "Stores",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Map",
+                table: "Stores");
+        }
+    }
+}
