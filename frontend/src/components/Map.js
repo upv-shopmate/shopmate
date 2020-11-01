5d26964117132bce4f39da1ebd7c09546860f1ab
Pere Díaz <pere-altea@hotmail.com>
2020-11-01 14:50:46 +0100

    default map creation and render

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,11 +1,19 @@
 import '../assets/css/Map.css';
 import React from 'react';
 
+// 853x566
+// 85x56
 class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'map': props.map,
+    }
+  }
   render() {
     return (
       <div className="map">
-          map
+        {this.state.map}
       </div>
     );
   }
--- frontend/src/components/RightPanel.js
+++ frontend/src/components/RightPanel.js
@@ -6,6 +6,7 @@ import Catalog from './Catalog';
 import Cart from './Cart';
 import Map from './Map';
 import Searcher from './SearchPanel';
+import Square from './Square'
 
 // minimum width is 70
 const WIDTHS = {
@@ -19,6 +20,7 @@ class RightPanel extends React.Component {
     super(props);
     this.state = {
       'componentDidMount': false,
+      'map': [],
     };
     this.currentPanel = this.currentPanel.bind(this);
   }
@@ -26,6 +28,67 @@ class RightPanel extends React.Component {
   componentDidMount() {
     this.setState({
       'componentDidMount': true,
+    });
+    this.initializeMap();
+  }
+
+  /**
+   * We compute squares before calling map
+   * 1 negro(no racismo) -> fondo
+   * 0 blanco -> espacios
+   * 2 gris -> cajas(pagar)
+   * 3 azul -> pescao
+   * 4 rojo -> charcuteria
+   * 5 verde -> verduras
+   * 6 marrón -> el resto
+   */
+  initializeMap() {
+    let squares = [],
+      width =  Math.floor(853 / 10),
+      height = Math.floor(566 / 10);
+    for(let x = 0; x < height; x++) {
+      squares[x] = []
+      for(let y = 0; y < width; y++) {
+        squares[x][y] = 0;
+      }
+    }
+    //cajas
+    squares = this.drawRectangle(squares, 45, 10, 56, 15, 2);
+    squares = this.drawRectangle(squares, 45, 16, 56, 21, 2);
+    squares = this.drawRectangle(squares, 45, 22, 56, 27, 2);
+
+    //izq
+    squares = this.drawRectangle(squares, 5, 0, 40, 3, 4);
+    squares = this.drawRectangle(squares, 0, 3, 5, 15, 4);
+    squares = this.drawRectangle(squares, 0, 15, 5, 30, 6);
+    squares = this.drawRectangle(squares, 10, 10, 40, 15, 4);
+    squares = this.drawRectangle(squares, 10, 20, 40, 25, 6);
+    //der
+    squares = this.drawRectangle(squares, 30, 30, 56, 85, 1);
+    squares = this.drawRectangle(squares, 20, 35, 25, 80, 5);
+    squares = this.drawRectangle(squares, 10, 35, 15, 80, 6);
+    squares = this.drawRectangle(squares, 2, 65, 7, 80, 3);
+    squares = this.drawRectangle(squares, 2, 45, 7, 60, 3);
+
+    this.drawMap(squares)
+  }
+  drawRectangle(squares, x_ini, y_ini, x_fi, y_fi, color) {
+    for(let i = x_ini; i < x_fi; i++) {
+      for(let j = y_ini; j < y_fi; j++) {
+        squares[i][j] = color;
+        console.log(i + "," + j);
+      } 
+    }
+    return squares;
+  }
+  drawMap(map) {
+    for(let x = 0; x < map.length; x++) {
+      for(let y = 0; y < map[x].length; y++) {
+        map[x][y] = <Square color={map[x][y]} x={x} y={y}/>;
+      }
+    }
+    this.setState({
+      'map': map,
     });
   }
 
