1666339e1d110d08d3cfeaf295cb5dcf6deaf3b3
Pere Díaz <pere-altea@hotmail.com>
2020-11-03 14:23:10 +0100

    map with endpoint

--- frontend/src/components/RightPanel.js
+++ frontend/src/components/RightPanel.js
@@ -7,6 +7,7 @@ import Cart from './Cart';
 import Map from './Map';
 import Searcher from './SearchPanel';
 import Square from './Square'
+import { requestMap } from '../requests/MapRequest';
 
 // minimum width is 70
 const WIDTHS = {
@@ -32,53 +33,9 @@ class RightPanel extends React.Component {
     this.initializeMap();
   }
 
-  /**
-   * We compute squares before calling map
-   * 1 negro(no racismo) -> fondo
-   * 0 blanco -> espacios
-   * 2 gris -> cajas(pagar)
-   * 3 azul -> pescao
-   * 4 rojo -> charcuteria
-   * 5 verde -> verduras
-   * 6 marrón -> el resto
-   */
-  initializeMap() {
-    let squares = [],
-      width =  Math.floor(853 / 10),
-      height = Math.floor(566 / 10);
-    for(let x = 0; x < height; x++) {
-      squares[x] = []
-      for(let y = 0; y < width; y++) {
-        squares[x][y] = 0;
-      }
-    }
-    //cajas
-    squares = this.drawRectangle(squares, 45, 10, 56, 15, 2);
-    squares = this.drawRectangle(squares, 45, 16, 56, 21, 2);
-    squares = this.drawRectangle(squares, 45, 22, 56, 27, 2);
-
-    //izq
-    squares = this.drawRectangle(squares, 5, 0, 40, 3, 4);
-    squares = this.drawRectangle(squares, 0, 0, 5, 15, 4);
-    squares = this.drawRectangle(squares, 0, 15, 5, 30, 6);
-    squares = this.drawRectangle(squares, 10, 10, 40, 15, 4);
-    squares = this.drawRectangle(squares, 10, 20, 40, 25, 6);
-    //der
-    squares = this.drawRectangle(squares, 30, 30, 56, 85, 1);
-    squares = this.drawRectangle(squares, 20, 35, 25, 80, 5);
-    squares = this.drawRectangle(squares, 10, 35, 15, 80, 6);
-    squares = this.drawRectangle(squares, 2, 65, 7, 80, 3);
-    squares = this.drawRectangle(squares, 2, 45, 7, 60, 3);
-
-    this.drawMap(squares)
-  }
-  drawRectangle(squares, x_ini, y_ini, x_fi, y_fi, color) {
-    for(let i = x_ini; i < x_fi; i++) {
-      for(let j = y_ini; j < y_fi; j++) {
-        squares[i][j] = color;
-      } 
-    }
-    return squares;
+  async initializeMap() {
+    const map = await requestMap();
+    this.drawMap(map)
   }
   drawMap(map) {
     for(let x = 0; x < map.length; x++) {
@@ -90,7 +47,12 @@ class RightPanel extends React.Component {
         }
         let needs_shadow = false;
         if(right == 0 || down == 0) needs_shadow = true; 
-        map[x][y] = <Square shadow={needs_shadow} color={map[x][y]} x={x} y={y}/>;
+        map[x][y] = (
+          <Square
+            key={(x, y)}
+            shadow={needs_shadow}
+            color={map[x][y]} x={x} y={y}
+        />);
       }
     }
     this.setState({
