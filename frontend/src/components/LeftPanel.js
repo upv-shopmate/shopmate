675ba987dee4d4c8f93fb73f926b8669252fbcd4
Sarwol <davit_96_28@hotmail.com>
2020-11-10 18:35:34 +0100

    Panel when user is not logged in, created (beta)

--- frontend/src/components/LeftPanel.js
+++ frontend/src/components/LeftPanel.js
@@ -1,10 +1,19 @@
 import '../assets/css/LeftPanel.css';
 import React from 'react';
+import NotLoginPanel from './NotLoginPanel';
 
 class LeftPanel extends React.Component {
   render() {
     return (
       <div className="left-panel">
+        <div className="left-panel-title">Lista de la compra</div>
+        <div className="current-panel">
+          <NotLoginPanel/>
+        </div>
+        <div className="bottom-buttons">
+          <button className="button">MIS LISTAS</button>
+          <button className ="button">CUPONES</button>   
+        </div>
       </div>
     );
   }
---
+++ frontend/src/components/NotLoginPanel.js
@@ -0,0 +1,24 @@
+import '../assets/css/NotLoginPanel.css';
+import accountImage from '../assets/images/account_circle.png';
+import React from 'react';
+
+class NotLoginPanel extends React.Component{
+    constructor(props) {
+        super(props);
+      }
+
+      render() {
+        return (
+            <div>
+                <div className="not-logged-user-block">
+                    <img className ="account-image" src={accountImage}></img>
+                    <div className="question"> ¿Ya tienes cuenta de cliente? </div>
+                    <div className="login-text"> Inicia sesión para ver tus listas de la compra, cupones y ofertas personalizadas</div>
+                    <button className="login-button">INICIAR SESIÓN</button>
+                </div>
+            </div>
+        );
+      }
+    }
+    
+    export default NotLoginPanel;
