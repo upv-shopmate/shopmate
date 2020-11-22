f6f1c5e3504a66144c1b6ffa234b0fdfd3c9106b
SaltyKawaiiNeko <software@skneko.moe>
2020-11-22 19:03:25 +0100

    Add DB migration for relating shopping lists to a owner user

---
+++ backend/ShopMate/Migrations/20201122180127_UserShoppingListsEndpoints.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopMate.Migrations
+{
+    public partial class UserShoppingListsEndpoints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "ShoppingLists",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingLists_OwnerId",
+                table: "ShoppingLists",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingLists_Users_OwnerId",
+                table: "ShoppingLists",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingLists_Users_OwnerId",
+                table: "ShoppingLists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingLists_OwnerId",
+                table: "ShoppingLists");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "ShoppingLists");
+        }
+    }
+}
