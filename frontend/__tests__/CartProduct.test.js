28b28a685f7e2f60a6ed8455aca8d24c0a46a061
Pere Díaz <pere-altea@hotmail.com>
2020-12-01 19:26:22 +0100

    test 2408

--- frontend/__tests__/CartProduct.test.js
+++ frontend/__tests__/CartProduct.test.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {shallow, configure, } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 import CartProduct from '../src/components/CartProduct'
 
-test('unitary price', () => {
+test('unitary price, quantity == 1', () => {
     const entry = {
         item: {
             pictures: [""],
             name: "foo",
-            barcode: "xdfedfwdf"
-        }
+            barcode: "xdfedfwdf",
+            priceWithVat: 1.4,
+        },
+        quantity: 1
     }
-    const component = renderer.create(
+    const component = shallow(
         <CartProduct entry={entry} key={entry.item.barcode}></CartProduct>
-    );
+    ).dive();
+    let priceDiv = component.find(".entrie-unit-price")
+    expect(priceDiv.text()).toEqual("")
+});
+
+test('unitary price, quantity > 1', () => {
+    const entry = {
+        item: {
+            pictures: [""],
+            name: "foo",
+            barcode: "xdfedfwdf",
+            priceWithVat: 1.4,
+        },
+        quantity: 2
+    }
+    const component = shallow(
+        <CartProduct entry={entry} key={entry.item.barcode}></CartProduct>
+    ).dive();
+    let priceDiv = component.find(".entrie-unit-price")
+    expect(priceDiv.text()).toEqual("1.40unitaryText") //40unitaryText  because of translation
 });
