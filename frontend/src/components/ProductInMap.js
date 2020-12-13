82509282563657c74fc816248af0f56204799d51
Pere Díaz <pere-altea@hotmail.com>
2020-12-13 16:21:45 +0100

    top product arrow points upwards

--- frontend/src/components/ProductInMap.js
+++ frontend/src/components/ProductInMap.js
@@ -2,51 +2,61 @@
 import '../assets/css/ProductInMap.css';
 import React from 'react';
 import { Store } from '../utils/Store.js'
-import {Bounce} from 'react-reveal'
+import { Bounce } from 'react-reveal'
 
 class ProductInMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-    this.waterDrop = React.createRef();
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        this.waterDrop = React.createRef();
+    }
 
-  componentDidMount() {
-      let coor = this.getCoordinates();
-      let left = coor[0] + "px";
-      let top = coor[1] + "px";
-      this.waterDrop.current.style.left = left;
-      this.waterDrop.current.style.top = top;
-  }
+    componentDidMount() {
+        let coor = this.getCoordinates();
+        let left = coor[0] + "px";
+        let top = coor[1] + "px";
+        this.waterDrop.current.style.left = left;
+        this.waterDrop.current.style.top = top;
+    }
 
-  getCoordinates() {
-      let pos = this.props.pos;
-      let left = (pos[0] * 10) - 25;
-      if(left < 0) left = 0;
-      let top = (pos[1] * 10) - 50;
-      return [left, top];
-  }
-  
-  render() {
-    return (
-        <div className="product-in-map">
-            <div 
-                className="product-in-map-wrapper"
-                ref={this.waterDrop}
-            >
-                <Bounce top>
-                    <img
-                        src={this.props.image}
-                        className="product-in-map-image"
-                    >
-                    </img>
-                    <div className="product-in-map-drop"></div>
-                </Bounce>
+    getCoordinates() {
+        let pos = this.props.pos;
+        let left = (pos[0] * 10) - 25;
+        if (left < 0) left = 0;
+        let top = (pos[1] * 10) - 50;
+        if (top < 40) {
+            let newTriangle = document.createElement('div');
+            newTriangle.setAttribute("id", "product-in-map-drop-top");
+            this.waterDrop.current.children[1].remove()
+            this.waterDrop.current.insertBefore(
+                newTriangle,
+                this.waterDrop.current.children[0],
+
+            );
+            top = 10;
+        }
+        return [left, top];
+    }
+    render() {
+        return (
+            <div className="product-in-map">
+                <div
+                    className="product-in-map-wrapper"
+                    ref={this.waterDrop}
+                >
+                    <Bounce top>
+                        <img
+                            src={this.props.image}
+                            className="product-in-map-image"
+                        >
+                        </img>
+                        <div id="product-in-map-drop"></div>
+                    </Bounce>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default ProductInMap;
